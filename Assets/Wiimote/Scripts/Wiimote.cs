f3bbdbcae73f62f116d62c2e2c4ff4c933454555
danger9819 <280948719+danger9819@users.noreply.github.com>
2026-05-01 18:09:45 +0100

    Don't warn when activating wii motion plus directly if using a Wii Remote Plus.
    I'm not sure if this is an the intended design of the API, or just a bug, but it appears that RequestIdentifyWiiMotionPlus (and RespondIdentifyWiiMotionPlus) doesn't return true if using a Wii Remote Plus.
    
    Directly calling ActivateWiiMotionPlus does work, but it warns if wmp_attached wasn't set to true (that is what makes me not certain that this is intentional).
    
    Assuming that it is intended design, a check for Wiimote Pluses seems like the best way to fix the issue.

--- Assets/Wiimote/Scripts/Wiimote.cs
+++ Assets/Wiimote/Scripts/Wiimote.cs
@@ -355,7 +355,7 @@ public class Wiimote
     /// If there is no Wii Motion Plus connected, undefined behavior may occur on the Wii Remote.
     public bool ActivateWiiMotionPlus()
     {
-        if (!wmp_attached)
+        if (!wmp_attached && _Type != WiimoteType.WIIMOTEPLUS)
             Debug.LogWarning("There is a request to activate the Wii Motion Plus even though it has not been confirmed to exist!  Trying anyway.");
 
         // Initialize the Wii Motion Plus by writing 0x55 to register 0xA600F0
